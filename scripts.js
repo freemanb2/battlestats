81ca15484d7c098326ae47d5581bebe3a46f5944
freemanb2 <34046697+freemanb2@users.noreply.github.com>
2017-12-08 17:46:44 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,40 +3,43 @@
 function updatePage(responseText){
     $('#json').html(responseText + '<hr>');
     var jsondata = JSON.parse(responseText);
-	addMatch(jsondata);
+	for(var i = 0; i < jsondata.data.length; i++){
+		addMatch(jsondata, i);
+	}
 }
 
-function addMatch(data){
-	$("#games").append("<div class='match'>\
-						<h2 class='title'>2v2 Matchmaking</h2>\
-						<hr><div class='team1'>\
+function addMatch(data, matchnum, matchid){
+	$("#wrapper").append("<div class='match-"+matchnum+"'>\
+						<h2 class='title'>Match "+(matchnum+1)+"</h2>\
+						<hr><div class='team1' id='team1-"+matchnum+"'>\
 						<h3>Team 1</h3>\
 						</div>\
-						<div class='team2'>\
+						<div class='team2' id='team2-"+matchnum+"'>\
 						<h3>Team 2</h3>\
 						</div>");
 	for(var i = 0; i < data.included.length; i++){
-        if(data.included[i].type == "participant"){
+        if(data.included[i].type == "participant" && data.included[i].attributes.stats.userID){
 			if(data.included[i].attributes.stats.side == 1){
-				addPlayer(data.included[i], ".team1");
+				addPlayer(data.included[i], "#team1-"+matchnum);
 			}
 			else{
-				addPlayer(data.included[i], ".team2");
+				addPlayer(data.included[i], "#team2-"+matchnum);
 			}
 		}
     }
-	$("#games").append("</div>"); //closing match div
+	$("#wrapper").append("</div>"); //closing match div
 }
 
-function addPlayer(data, team, player){
-	$(team).append("<div class='player'>");
-	$(team).append("Player ID: " + data.attributes.stats.userID + "<br>");
-	$(team).append("Character: " + data.attributes.actor + "<br>");
-	$(team).append("Kills: " + data.attributes.stats.kills + "<br>");
-	$(team).append("Deaths: " + data.attributes.stats.deaths + "<br>");
-	$(team).append("Damage Done: " + data.attributes.stats.damageDone + "<br>");
-	$(team).append("Damage Taken: " + data.attributes.stats.damageReceived + "<br>");
-	$(team).append("</div>");
+function addPlayer(data, team){
+	alert("adding to "+team);
+	$(team).append("<div class='player'>\
+	Player ID: " + data.attributes.stats.userID + "<br>\
+	Character: " + data.attributes.actor + "<br>\
+	Kills: " + data.attributes.stats.kills + "<br>\
+	Deaths: " + data.attributes.stats.deaths + "<br>\
+	Damage Done: " + data.attributes.stats.damageDone + "<br>\
+	Damage Taken: " + data.attributes.stats.damageReceived + "<br>\
+	</div>");
 }
 
 
@@ -47,11 +50,12 @@ $( function() {
 		   updatePage(xhttp.responseText);
 		}
 	};
-	xhttp.open("GET", "https://api.dc01.gamelockerapp.com/shards/global/matches/AB9C81FABFD748C8A7EC545AA6AF97CC", true);
+	xhttp.open("GET", "https://api.dc01.gamelockerapp.com/shards/global/matches?filter[playerIds]=785877916079755264&page[limit]=2", true);
 	xhttp.setRequestHeader('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxYWU3OTI2MC1iNjAxLTAxMzUtMDllYS0wYTU4NjQ2MGYwOTUiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTExODI4OTYyLCJwdWIiOiJzdHVubG9jay1zdHVkaW9zIiwidGl0bGUiOiJiYXR0bGVyaXRlIiwiYXBwIjoiYmF0dGxlc3RhdHMtNGFlMjUwMjctMjgzMS00YzViLThmZTktNWFhMDRiYmIzYmZiIiwic2NvcGUiOiJjb21tdW5pdHkiLCJsaW1pdCI6MTB9.iweLw8k1BkH1lrSlsfKAo3nwbWwNu-Y9lmDSTeJ_qZA');
 	xhttp.setRequestHeader('Accept', 'application/vnd.api+json');
 	xhttp.send();
     $( "#accordion" ).accordion({
-      collapsible: true
+      collapsible: true,
+	  active: false
     });
   } );
